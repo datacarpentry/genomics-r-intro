9174bc0aefe46483ce2203aaa3aa3f07834301f0
François Michonneau <francois.michonneau@gmail.com>
2018-10-03 13:20:07 -0400

    try to delete the .gitignore

--- build_lesson.R
+++ build_lesson.R
@@ -8,6 +8,7 @@ BuildLesson <- R6::R6Class(
       if (build_status > 0)
         stop("Error during building process")
 
+      file.remove(".gitignore")
       system("mkdir  _rendered")
       system("cp -r \`ls -A | grep -v '.git' | grep -v '_rendered' | grep -v '_site'\` _rendered")
     })
