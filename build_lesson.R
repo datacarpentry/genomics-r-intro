fefb5ccfffb6eadf9a898c5f303abe2fd98fd692
François Michonneau <francois.michonneau@gmail.com>
2020-06-24 11:35:20 +0200

    update CI

--- build_lesson.R
+++ build_lesson.R
@@ -1,5 +1,4 @@
 library(tic)
-
 BuildLesson <- R6::R6Class(
   "BuildLesson", inherit = TicStep,
   public = list(
@@ -14,59 +13,52 @@ BuildLesson <- R6::R6Class(
     })
 )
 
-
 build_lesson <- function() {
-    BuildLesson$new()
+  BuildLesson$new()
 }
 
-
-format_url_errors <- function(res_links, idx) {
-    paste0("  - broken url:", res_links$urlname[idx], "\n",
-           "          from:", res_links$parentname[idx], "\n")
-}
 
 CheckLinks <- R6::R6Class(
-     "CheckLinks", inherit = TicStep,
-     public = list(
-       run = function() {
-         ## While build_lesson() merely copies the content of the repo into a
-         ## new folder that GitHub picks up to render (so the dynamically
-         ## generated links such as "Edit on GitHub" are functional), here we
-         ## actually need to generate the website so we can test the links.
-         on.exit(system("rm -rf _rendered/_site"))
+  "CheckLinks", inherit = TicStep,
+  public = list(
+    run = function() {
+      ## While build_lesson() merely copies the content of the repo into a
+      ## new folder that GitHub picks up to render (so the dynamically
+      ## generated links such as "Edit on GitHub" are functional), here we
+      ## actually need to generate the website so we can test the links.
+      on.exit(system("rm -rf _rendered/_site"))
 
-         system("cd _rendered && rvm 2.5.1 do ruby -S jekyll build")
+      system("cd _rendered && rvm 2.5.3 do ruby -S jekyll build")
 
-         ## ignore JS file not included as part of rmarkdown
-         ## ignore email addresses
-         ## ignore embedded images
-         link_status <- system("linkchecker --ignore-url=external.+js --ignore-url=^mailto: --ignore-url=^data: --no-warnings  --file-output=csv/link_res.csv _rendered/_site")
-         message("linkchecker exit code: ", link_status)
+      res <- checker::check_links("_site",
+        show_summary = TRUE,
+        check_external = TRUE,
+        only_with_issues = FALSE,
+        ignore_pattern = "site_libs"
+      )
 
-         ## Because URLs can contain #, we first need to remove the commented
-         ## lines in the generated CSV file. We can't rely on the comment
-         ## argument of the read_delim function, see
-         ## https://github.com/tidyverse/readr/issues/828
-         csv_links_files <- readLines("link_res.csv")
-         writeLines(csv_links_files[grepl("^[^#]", csv_links_files)],
-                    con = "link_res_clean.csv",
-                    sep = "\n")
-
-         ## write output to CSV file and check error codes
-         ## stop only for 404s
-         res_links <- readr::read_delim("link_res_clean.csv", delim = ";")
-         unique(res_links$result)
-         res_404 <- grepl("^404|^gaierror", res_links$result)
-         other_errors <- grepl("error", res_links$result, ignore.case = TRUE)
-         if (any(other_errors))
-           warning("These links might be problematic: \n",
-                   format_url_errors(res_links, other_errors))
-         if (any(res_404))
-           stop("Some links are broken (check log to inspect): \n",
-                format_url_errors(res_links, res_404))
-         })
+      res
+    })
 )
 
 check_links <- function() {
   CheckLinks$new()
+}
+
+cfg  <- rprojroot::has_file_pattern("^_config.y*ml$")
+root <- rprojroot::find_root(cfg)
+
+required_pkgs <- unique(c(
+  ## Packages for episodes
+  renv::dependencies(file.path(root, "_episodes_rmd"), progress = FALSE, error = "ignore")$Package,
+  ## Pacakges for tools
+  renv::dependencies(file.path(root, "bin"), progress = FALSE, error = "ignore")$Package
+))
+
+missing_pkgs <- setdiff(required_pkgs, rownames(installed.packages()))
+
+if (length(missing_pkgs)) {
+  message("Installing missing required packages: ",
+    paste(missing_pkgs, collapse=", "))
+  install.packages(missing_pkgs)
 }
--- tic.R
+++ tic.R
@@ -1,17 +1,16 @@
 source("build_lesson.R")
 
 get_stage("before_install") %>%
-  add_code_step(install.packages("readr")) %>%
+  add_code_step(install.packages("git2r")) %>%
   add_code_step(update.packages(ask = FALSE))
 
 get_stage("install") %>%
-  add_code_step(remotes::install_github("hadley/requirements")) %>%
-  add_code_step(install.packages(requirements:::req_dir("_episodes_rmd")))
+  add_code_step(system("python -m pip install update-copyright")) %>%
+  add_code_step(remotes::install_deps(dependencies = TRUE)) %>%
+  add_step(step_install_github("fmichonneau/checker"))
 
 get_stage("deploy") %>%
-  add_step(build_lesson())
-
-get_stage("after_deploy") %>%
+  add_step(build_lesson()) %>%
   add_step(check_links())
 
 if (Sys.getenv("id_rsa") != "") {
